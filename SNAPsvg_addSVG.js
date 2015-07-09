bcdc3cf5eb1c89db9198f10c3ef45a9d6fb4ac2c
Christian Münch <c.muench@outlook.de>
2015-07-10 00:01:02 +0200

    First Commit
    A function that (hopefully) makes it easier to add svgs and apply basic
    styling via the SNAP library. Feel free to use

---
+++ SNAPsvg_addSVG.js
@@ -0,0 +1,19 @@
+// this function makes it easy to add an external SVG and apply some basic styling to it. 
+// you can append the SVg to a svg or div tag via its id.
+
+// this is an example of how to call the function:
+// addSVG("exampleSVG","#testID_01","red","blue",0.5);
+
+
+function addSVG(svgSource,appendTo,strokeColor,fillColor,fillOpacity)	{
+	var s = Snap(appendTo);
+	Snap.load(svgSource, function (f) {
+
+	    f.selectAll("line").attr({fill:fillColor, "fill-opacity":fillOpacity, stroke:strokeColor});
+	    f.selectAll("path").attr({fill:fillColor, "fill-opacity":fillOpacity, stroke:strokeColor});
+	    f.selectAll("polygon").attr({fill:fillColor, "fill-opacity":fillOpacity, stroke:strokeColor});
+	    f.selectAll("polyline").attr({stroke: strokeColor, stroke:strokeColor});
+
+	    s.append(f.select("g"));
+	});
+}
